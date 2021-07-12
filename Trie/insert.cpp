775ce8119375c4da88b8861acdd5966f74a6dc5c
edenpl <64106485+edenpl@users.noreply.github.com>
2021-07-12 18:04:10 -0500

    datos

---
+++ Trie/insert.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cstdlib>
+#include<fstream>
+using namespace std;
+fstream archivo;
+void read_file(){
+    archivo.open("datos.txt",ios::in);
+    if (archivo.is_open()) {
+        string a;
+        while ( !archivo.eof() )
+        {/*
+            a = archivo.get();
+            cout<<a<<endl;*/
+        }
+        archivo.close();
+    }
+    else
+    {
+        cout<<"Archivo no encontrado";
+    }
+}
+int main(){
+    read_file();
+    return 0;
+}
